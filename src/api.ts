37203b5625c107105c7fec6e46d8d94d0554de18
Lucia <hyeyoung9115@gmail.com>
2022-08-21 00:54:15 +0900

    ♻️ Add fetcher functions file

---
+++ src/api.ts
@@ -0,0 +1,19 @@
+import { response } from "express";
+
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+export function fetchCoins() {
+  return fetch(`${BASE_URL}/tickers`).then((response) => response.json());
+}
+
+export function fetchCoinInfo(coinId: string | undefined) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinTickers(coinId: string | undefined) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
+  );
+}
